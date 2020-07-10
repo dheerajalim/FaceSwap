dba7c7a7633fe95c76c154405d572d33c50a5bed
dheeraj <dheeraj.alim@gmail.com>
2020-07-11 02:33:26 +0530

    handle video feed for face swap

--- FaceSwap/faceswap_video.py
+++ FaceSwap/faceswap_video.py
@@ -11,85 +11,106 @@ from drawing import *
 import FaceRendering
 import utils
 
+import settings_storage
+import wx
+import tkinter
+import tkinter.filedialog as fd
+import os
+import time
+
 # print ("Press T to draw the keypoints and the 3D model"
 # print "Press R to start recording to a video file"
 
-#you need to download shape_predictor_68_face_landmarks.dat from the link below and unpack it where the solution file is
-#http://sourceforge.net/projects/dclib/files/dlib/v18.10/shape_predictor_68_face_landmarks.dat.bz2
+def faceswap():
 
-#loading the keypoint detection model, the image and the 3D model
-predictor_path = "E:/Development/faceswap/FaceSwap/models/shape_predictor_68_face_landmarks.dat"
-image_name = "../data/rekha.jpg"
-#the smaller this value gets the faster the detection will work
-#if it is too small, the user's face might not be detected
-maxImageSizeForDetection = 320
+    source_image_path, destination_image_path, dlib_model, output_video_path, save_image, save_video = \
+        settings_storage.fetch_settings()
+    #loading the keypoint detection model, the image and the 3D model
+    predictor_path = os.getcwd()+"\\models\\"+dlib_model
+    image_name = source_image_path
+    #the smaller this value gets the faster the detection will work
+    #if it is too small, the user's face might not be detected
+    maxImageSizeForDetection = 320
 
-detector = dlib.get_frontal_face_detector()
-predictor = dlib.shape_predictor(predictor_path)
-mean3DShape, blendshapes, mesh, idxs3D, idxs2D = utils.load3DFaceModel("../candide.npz")
+    detector = dlib.get_frontal_face_detector()
+    predictor = dlib.shape_predictor(predictor_path)
+    mean3DShape, blendshapes, mesh, idxs3D, idxs2D = utils.load3DFaceModel("../candide.npz")
 
-projectionModel = models.OrthographicProjectionBlendshapes(blendshapes.shape[0])
+    projectionModel = models.OrthographicProjectionBlendshapes(blendshapes.shape[0])
 
-modelParams = None
-lockedTranslation = False
-drawOverlay = False
-# cap = cv2.VideoCapture('http://192.168.29.156:4747/video')
-cap = cv2.VideoCapture(0)
-writer = None
-cameraImg = cap.read()[1]
-
-textureImg = cv2.imread(image_name)
-textureCoords = utils.getFaceTextureCoords(textureImg, mean3DShape, blendshapes, idxs2D, idxs3D, detector, predictor)
-renderer = FaceRendering.FaceRenderer(cameraImg, textureImg, textureCoords, mesh)
-
-while True:
+    modelParams = None
+    lockedTranslation = False
+    drawOverlay = False
+    # cap = cv2.VideoCapture('http://192.168.29.156:4747/video')
+    cap = cv2.VideoCapture(0)
+    writer = None
     cameraImg = cap.read()[1]
-    shapes2D = utils.getFaceKeypoints(cameraImg, detector, predictor, maxImageSizeForDetection)
 
-    if shapes2D is not None:
-        for shape2D in shapes2D:
-            #3D model parameter initialization
-            modelParams = projectionModel.getInitialParameters(mean3DShape[:, idxs3D], shape2D[:, idxs2D])
+    textureImg = cv2.imread(image_name)
+    textureCoords = utils.getFaceTextureCoords(textureImg, mean3DShape, blendshapes, idxs2D, idxs3D, detector, predictor)
+    renderer = FaceRendering.FaceRenderer(cameraImg, textureImg, textureCoords, mesh)
 
-            #3D model parameter optimization
-            modelParams = NonLinearLeastSquares.GaussNewton(modelParams, projectionModel.residual, projectionModel.jacobian, ([mean3DShape[:, idxs3D], blendshapes[:, :, idxs3D]], shape2D[:, idxs2D]), verbose=0)
+    while True:
+        cameraImg = cap.read()[1]
+        shapes2D = utils.getFaceKeypoints(cameraImg, detector, predictor, maxImageSizeForDetection)
 
-            #rendering the model to an image
-            shape3D = utils.getShape3D(mean3DShape, blendshapes, modelParams)
-            renderedImg = renderer.render(shape3D)
+        if shapes2D is not None:
+            for shape2D in shapes2D:
+                #3D model parameter initialization
+                modelParams = projectionModel.getInitialParameters(mean3DShape[:, idxs3D], shape2D[:, idxs2D])
 
-            #blending of the rendered face with the image
-            mask = np.copy(renderedImg[:, :, 0])
-            renderedImg = ImageProcessing.colorTransfer(cameraImg, renderedImg, mask)
-            cameraImg = ImageProcessing.blendImages(renderedImg, cameraImg, mask)
-       
+                #3D model parameter optimization
+                modelParams = NonLinearLeastSquares.GaussNewton(modelParams, projectionModel.residual, projectionModel.jacobian, ([mean3DShape[:, idxs3D], blendshapes[:, :, idxs3D]], shape2D[:, idxs2D]), verbose=0)
 
-            #drawing of the mesh and keypoints
-            if drawOverlay:
-                drawPoints(cameraImg, shape2D.T)
-                drawProjectedShape(cameraImg, [mean3DShape, blendshapes], projectionModel, mesh, modelParams, lockedTranslation)
+                #rendering the model to an image
+                shape3D = utils.getShape3D(mean3DShape, blendshapes, modelParams)
+                renderedImg = renderer.render(shape3D)
 
-    if writer is not None:
-        writer.write(cameraImg)
+                #blending of the rendered face with the image
+                mask = np.copy(renderedImg[:, :, 0])
+                renderedImg = ImageProcessing.colorTransfer(cameraImg, renderedImg, mask)
+                cameraImg = ImageProcessing.blendImages(renderedImg, cameraImg, mask)
 
-    cv2.imshow('image', cameraImg)
-    key = cv2.waitKey(1)
 
-    if key == 27:
-        break
-    if key == ord('t'):
-        drawOverlay = not drawOverlay
-    if key == ord('r'):
-        if writer is None:
-            # print "Starting video writer"
-            writer = cv2.VideoWriter("../out.avi", cv2.cv.CV_FOURCC('X', 'V', 'I', 'D'), 25, (cameraImg.shape[1], cameraImg.shape[0]))
+                #drawing of the mesh and keypoints
+                if drawOverlay:
+                    drawPoints(cameraImg, shape2D.T)
+                    drawProjectedShape(cameraImg, [mean3DShape, blendshapes], projectionModel, mesh, modelParams, lockedTranslation)
 
-            if writer.isOpened():
-                print("Writer succesfully opened")
-            else:
-                writer = None
-                print("Writer opening failed")
-        else:
-            print("Stopping video writer")
-            writer.release()
-            writer = None
+        if writer is not None:
+            writer.write(cameraImg)
+
+        cv2.imshow('Face Swap', cameraImg)
+        key = cv2.waitKey(1)
+
+        if key == 27:
+            if save_video == 2:
+                print("Stopping video writer")
+                writer.release()
+            break
+
+        if cv2.getWindowProperty('Face Swap', cv2.WND_PROP_VISIBLE) < 1:
+            if save_video == 2:
+                print("Stopping video writer")
+                writer.release()
+            break
+
+        if key == ord('t'):
+            drawOverlay = not drawOverlay
+        # if key == ord('r'):
+        if save_video == 1:
+            if writer is None:
+                # print "Starting video writer"
+                writer = cv2.VideoWriter(output_video_path+"//"+time.strftime("%Y%m%d-%H%M%S")+".avi", cv2.VideoWriter_fourcc('X', 'V', 'I', 'D'), 10, (cameraImg.shape[1], cameraImg.shape[0]))
+
+                if writer.isOpened():
+                    print("Writer succesfully opened")
+                else:
+                    writer = None
+                    print("Writer opening failed")
+                save_video = 2
+            # else:
+            #     print("Stopping video writer")
+            #     writer.release()
+            #     writer = None
+
